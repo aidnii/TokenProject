aa3c35f499b12f17eb33b9cd260b69d69bc1ee95
cryptichic <syberchief@gmail.com>
2022-03-14 15:37:05 -0500

    adding constructor arguments

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -3,5 +3,15 @@ pragma solidity ^0.8.6;
 
 contract Token {
     string public name = "AIDNI";
+    string public symbol = "AID";
+    uint256 public decimals = 18;
+    uint256 public totalSupply = 1000000000000000000;
+
+    constructor(string memory _name, string memory _symbol, uint _decimals, uint _totalSupply){
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+        totalSupply = _totalSupply;
+    }
 }
 
