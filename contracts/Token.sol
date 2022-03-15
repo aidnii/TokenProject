7afb60fbaaf08920f908796c596d49b8ade5b58d
cryptichic <syberchief@gmail.com>
2022-03-14 19:02:41 -0500

    adding comments and the approved functionality

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -2,14 +2,20 @@
 pragma solidity ^0.8.6;
 
 contract Token {
+    
+    // The variables
     string public name = "AIDNI";
     string public symbol = "AID";
     uint256 public decimals = 18;
     uint256 public totalSupply = 1000000000000000000000000;
 
+   // Keeping track of balances and approved allowances
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
-    event Transfer(address indexed from, address indexed to, uint256 value);
+    // Events - creating events on state changes
+    event Transfer(address indexed from, address indexed to, uint256 value); 
+    event Approval(address indexed owner, address indexed spender, uint256 value);
 
     constructor(string memory _name, string memory _symbol, uint _decimals, uint _totalSupply){
         name = _name;
@@ -19,9 +25,16 @@ contract Token {
         balanceOf[msg.sender] = totalSupply;
     }
 
-    function transfer(address _from, address _to, uint256 _value) external returns (bool success) {
-        balanceOf[msg.sender] = balanceOf[_from] - (_value);
+    /// @notice transfer amount of tokens to an address
+    /// @param _to receiver of token
+    /// @param _value amount value of token to send
+    /// @return success as true, for transfer
+    function transfer(address _to, uint256 _value) external returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender] = balanceOf[msg.sender] - (_value);
         balanceOf[_to] = balanceOf[_to] + (_value);
+        emit Transfer(msg.sender, _to, _value);
+        return true;
     }
 }
 
